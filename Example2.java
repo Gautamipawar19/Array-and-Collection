5ad8dce55ca8e1a8ddf3c8dada428d03efd01c4f
Gautamipawar19 <gautamipawar19@gmail.com>
2025-08-27 21:12:43 +0530

    Arrays

---
+++ Example2.java
@@ -0,0 +1,15 @@
+class Example2
+{
+	public static void main(String args[])
+	{
+		int[] a= new int[3];
+		System.out.println(a.length);
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		for (int i=0; i<a.length; i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+}

b5e5a36f7db9d61d0b23a16c86eb01a27ba800a5
Gautamipawar19 <gautamipawar19@gmail.com>
2025-08-27 21:12:00 +0530

    Arrays
    An array is a container object that holds a fixed number of values of the same type.

---
+++ InstanceOfOperator.java
@@ -0,0 +1,15 @@
+class InstanceOfOperator
+{
+	public static void main(String args[])
+	{
+		String str=null;
+		if(str instanceof String)
+		{
+			System.out.println("Str is String type");
+		}
+		else
+		{
+			System.out.println("Str is not String type");
+		}
+	}
+}
